1525f977910db08b5d80b593f0fc930e8f07ee70
client2233 <zs987526341@gmail.com>
2025-03-16 22:31:31 +0800

    get avatar

--- pages/notification/notification.js
+++ pages/notification/notification.js
@@ -2,17 +2,14 @@ Page({
     data:{
         likesUsers:[],
         favouriteUsers: [],
-        comment: []
-    },
-
-    onLoad() {
-        console.log("加载notification")
-        this.loadNotification();
+        comment: [],
+        likesUsersAvatar:[],
+        likesUsersName:[]
     },
 
     async loadNotification(){
         try{
-            let openId = "";
+            var openId = "";
             try{
                 const wxContext = await wx.cloud.callFunction({
                     name: "getOpenID"
@@ -36,6 +33,61 @@ Page({
             console.log("加载notification失败");
         }
 
+        try{
+            wx.cloud.database().collection("notification").doc(openId).get()
+                .then(res => {
+                    for(let i=res.data.posts.length-1; i>=0; i--){
+                        for(let j=res.data.posts[i].likesUsers.length-1; j>=0; j--){
+                            this.data.likesUsers.push(res.data.posts[i].likesUsers[j]);
+                            this.setData({
+                               likesUsers: this.data.likesUsers
+                            });
+                        }
+                    }
 
+                    for (let i=0; i<this.data.likesUsers.length; i++) {
+                        wx.cloud.database().collection("users").where({
+                            openid: this.data.likesUsers[i]
+                        }).get()
+                            .then(result => {
+                                this.data.likesUsersAvatar.push(result.data[0].avatarUrl);
+                                this.setData({
+                                    likesUsersAvatar: this.data.likesUsersAvatar
+                                });
+                        })
+                    }
+
+                })
+                .catch(err => {
+                    console.log("拉取notification数据失败");
+                })
+        }catch (err){
+            console.log("失败");
+        }
+
+    },
+
+    //拉取用户头像
+    getLikesUsersAvatar(){
+      try{
+            for (let i=0; i<this.data.likesUsers.length; i++) {
+                wx.cloud.database().collection("users").where({
+                    openid: this.data.likesUsers[i]
+                }).get(res => {
+                    this.data.likesUsersAvatar.push(res.data.avatarUrl);
+                    this.setData({
+                        likesUsersAvatar: this.data.likesUsersAvatar
+                    });
+                })
+            }
+        }catch(err){
+            console.log("失败");
+        }
+    },
+
+    onLoad() {
+        console.log("加载notification")
+        this.loadNotification();
     }
+
 })
